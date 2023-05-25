917bc079c5a63904f599e2a76293256d6455bf84
Amili-usain <amilihussein073@gmail.com>
2023-05-25 13:01:13 +0000

    tasks 5 & 6

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Encrypting passwords module.
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Hashes a password using bcrypt."""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates if the provided password matches the hashed password."""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
